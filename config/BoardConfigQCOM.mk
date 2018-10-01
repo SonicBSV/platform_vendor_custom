4163caedb44c11f309861ebaf605e798ef57c6f0
Pedro Kalil <kalilgamer0@gmail.com>
2018-10-01 11:28:21 -0300

    hals: remove msm prefix on QCOM_TARGET_VARIANT * The hals are pushed to the branch 9.0-caf-8998 instead of 9.0-caf-msm8998 for instance

--- config/BoardConfigQCOM.mk
+++ config/BoardConfigQCOM.mk
@@ -51,30 +51,30 @@ MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 
 ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(B_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8974
+    QCOM_HARDWARE_VARIANT := 8974
 else
 ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(B64_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8994
+    QCOM_HARDWARE_VARIANT := 8994
 else
 ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(BR_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8916
+    QCOM_HARDWARE_VARIANT := 8916
 else
 ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(UM_3_18_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8996
+    QCOM_HARDWARE_VARIANT := 8996
 else
 ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(UM_4_4_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8998
+    QCOM_HARDWARE_VARIANT := 8998
 else
 ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
     MSM_VIDC_TARGET_LIST := $(UM_4_9_FAMILY)
     QCOM_HARDWARE_VARIANT := sdm845
 else
     MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
-    QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+    QCOM_HARDWARE_VARIANT := $(shell echo $(TARGET_BOARD_PLATFORM) | sed -e 's/msm//')
 endif
 endif
 endif
