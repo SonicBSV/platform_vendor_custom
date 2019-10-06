1a9d708ec75f3e36884c92579851274d4cf208a4
T1mpp4 <timi.rautamaki@gmail.com>
2019-10-06 14:12:49 +0200

    Add kernel.mk and BoardConfigKernel from Lineage
    Needs works to get working

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,3 +1,5 @@
+export SDLCANG_PATH=0
+export SDCLANG_PATH_2=0
 export SDCLANG=false
 export SDCLANG_AE_CONFIG=vendor/custom/sdclang/sdclangAE.json
 export SDCLANG_CONFIG=vendor/custom/sdclang/sdclang.json
