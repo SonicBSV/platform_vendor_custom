d87530b71cedd033a1cc5c3eefa9344552fea3d7
T1mpp4 <T1mpp4@users.noreply.github.com>
2019-10-08 00:49:07 +0300

    Disable QPerformance
    2/2

--- config/qualcomm.mk
+++ config/qualcomm.mk
@@ -1,9 +1,5 @@
-LIBPERFLOCK := org.codeaurora.Performance
-
 PRODUCT_PACKAGES += \
     SnapdragonGallery \
     SnapdragonMusic \
     SnapdragonCamera \
     MusicFX
-
-PRODUCT_PACKAGES += $(LIBPERFLOCK)
