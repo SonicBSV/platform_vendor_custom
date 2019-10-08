9e986633f4dfe243151e6b3d343619e3ea774344
T1mpp4 <timi.rautamaki@gmail.com>
2019-10-08 14:18:10 +0200

    Add variable Uses_qcom_bsp_legacy

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -20,6 +20,9 @@ type Product_variables struct {
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
+	Uses_qcom_bsp_legacy struct {
+		Cppflags []string
+	}
 	Uses_qti_camera_device struct {
 		Cppflags []string
 		Shared_libs []string
@@ -37,6 +40,7 @@ type ProductVariables struct {
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Uses_qcom_bsp_legacy                  *bool   `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -7,6 +7,7 @@ $(call add_json_bool, Needs_text_relocations,               $(filter true,$(TARG
 $(call add_json_str,  Specific_camera_parameter_library,    $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str,  Target_shim_libs,                     $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library,$(if $(TARGET_CAMERA_PARAMETER_LIBRARY),,true))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_nvidia_enhancements,             $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy,                 $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device,               $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
