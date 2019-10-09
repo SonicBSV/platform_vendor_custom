808f48f03bec9cfb054edc3fa9daa0a1c4e0c183
T1mpp4 <timi.rautamaki@gmail.com>
2019-10-09 08:46:24 +0200

    Hardcode sdclang path for now

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,6 +1,1 @@
-export SDLCANG_PATH=$(pwd)/vendor/qcom/sdclang-8.0/linux-x86/bin
-export SDCLANG_PATH_2=$(pwd)/vendor/qcom/sdclang-8.0/linux-x86/bin
-export SDCLANG=true
-export SDCLANG_AE_CONFIG=$(pwd)/vendor/custom/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=$(pwd)/vendor/custom/sdclang/sdclang.json
-export SDCLANG_SA_ENABLED=false
+#
