b4eb0a88e332435ccee13a92b20b6e019786b68d
T1mpp4 <timi.rautamaki@gmail.com>
2019-10-21 09:29:07 +0200

    Update apns makefile

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := apns-conf.xml
 LOCAL_MODULE_CLASS := ETC
-DEFAULT_APNS_FILE := vendor/custom/prebuilt/common/etc/apns-conf.xml
-LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+LOCAL_PREBUILT_MODULE_FILE := vendor/custom/prebuilt/common/etc/apns-conf.xml
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
