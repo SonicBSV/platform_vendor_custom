0ac9a6589e15d3af114fe0a1188379087a22cae8
rautamak <13891897+rautamak@users.noreply.github.com>
2019-12-18 19:18:53 +0200

    vendor/custom: sdclang: use relative paths

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -1,5 +1,5 @@
 # SDClang Environment Variables
 export SDCLANG=true
-export SDCLANG_AE_CONFIG=$(pwd)/vendor/custom/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=$(pwd)/vendor/custom/sdclang/sdclang.json
+export SDCLANG_AE_CONFIG=vendor/custom/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=vendor/custom/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false
