43651f544327944a8fce1f4d899dc423b5e6a1ec
rautamak <13891897+rautamak@users.noreply.github.com>
2019-12-19 11:18:54 +0100

    vendor/custom: set sdclang paths in vendorsetup.sh

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -3,3 +3,6 @@ export SDCLANG=true
 export SDCLANG_AE_CONFIG=vendor/custom/sdclang/sdclangAE.json
 export SDCLANG_CONFIG=vendor/custom/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false
+
+export SDCLANG_PATH=vendor/qcom/sdclang-8.0/linux-x86/bin
+export SDCLANG_PATH_2=vendor/qcom/sdclang-8.0/linux-x86/bin
