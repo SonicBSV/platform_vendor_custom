3762541149167cef4729d04c02868d4d6cc7ec5d
rautamak <timi.rautamaki@gmail.com>
2020-05-31 21:31:05 +0300

    vendor/custom: import backuptools from LineageOS
    Change-Id: I6ec61475399797513ffd44fbd6fadf870f272a08

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -3,6 +3,3 @@ export SDCLANG=true
 export SDCLANG_AE_CONFIG=$(pwd)/vendor/custom/sdclang/sdclangAE.json
 export SDCLANG_CONFIG=$(pwd)/vendor/custom/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false
-
-export SDCLANG_PATH=$(pwd)/vendor/qcom/sdclang-8.0/linux-x86/bin
-export SDCLANG_PATH_2=$(pwd)/vendor/qcom/sdclang-8.0/linux-x86/bin
